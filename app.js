47a063c11a70567d6391c355147ebf77b7aabda5
JackieC1993 <jackiecheung@pursuit.org>
2023-06-27 21:18:10 -0400

    added image and remove button in index.html

--- app.js
+++ app.js
@@ -5,7 +5,14 @@ form.addEventListener("submit" , (e) =>{
     e.preventDefault()
     let name = form.querySelector("#name").value
     let price= form.querySelector("#price").value
-    let itemText= `name:${name} price:${price}`
+    let itemText= `<h3> name: </h3> <p> ${name} </p> 
+    <h3> price: </h3> <p> ${price} </p>`
+    let buttonText = '<button id ="new">remove</button>'
     let li = document.createElement("li")
-    
+    let button = document.createElement("button")
+    button.innerHTML= buttonText
+    li.innerHTML = itemText
+    ul.append(li)
+    li.append(button)
 })
+
